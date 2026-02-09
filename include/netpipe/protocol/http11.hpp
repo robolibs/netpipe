d2543cc236981e84424839a4dba8c0c1e7437fca
Trim Bresilla <trim.bresilla@gmail.com>
2026-02-09 16:16:46 +0100

    feat(http11): implement content-length and chunked body handling

--- include/netpipe/protocol/http11.hpp
+++ include/netpipe/protocol/http11.hpp
@@ -4,6 +4,7 @@
 
 #include <netpipe/protocol/http/common.hpp>
 #include <netpipe/protocol/http/transport_adapter.hpp>
+#include <netpipe/protocol/http11/body.hpp>
 #include <netpipe/protocol/http11/parser.hpp>
 #include <netpipe/protocol/http11/serialize.hpp>
 #include <netpipe/protocol/http11/types.hpp>
